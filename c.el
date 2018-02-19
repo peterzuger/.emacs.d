3f35946f3659ae3bb72b7ab5226fd1e60f295759
Peter Züger <zueger.peter@icloud.com>
2018-02-19 11:10:03 +0100

    removed extern C from auto indent

--- c.el
+++ c.el
@@ -2,6 +2,10 @@
 (setq c-default-style "k&r"
       c-basic-offset 4)
 
+(add-hook 'c-mode-common-hook
+	  (lambda()
+	    (c-set-offset 'inextern-lang 0)))
+
 (defun my-make-CR-do-indent ()
   (define-key c-mode-base-map "\C-m" 'c-context-line-break))
 (add-hook 'c-initialization-hook 'my-make-CR-do-indent)
