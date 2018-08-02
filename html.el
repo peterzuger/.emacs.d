7895517e7d2edfddeef68366926d4fe4e9955cf2
Peter Züger <zueger.peter@icloud.com>
2018-08-02 16:18:01 +0200

    added handlebars files to html mode

--- html.el
+++ html.el
@@ -1,4 +1,5 @@
 (add-hook 'html-mode-hook
-	  (lambda ()
-	    ;; Default indentation is usually 2 spaces, changing to 4.
-	    (set (make-local-variable 'sgml-basic-offset) 4)))
+          (lambda ()
+            ;; Default indentation is usually 2 spaces, changing to 4.
+            (set (make-local-variable 'sgml-basic-offset) 4)))
+(add-to-list 'auto-mode-alist '("handlebars" . html-mode))
