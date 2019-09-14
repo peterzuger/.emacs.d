83fb5da25443527b8644aca70a34de9dbcff4000
Peter Züger <zueger.peter@icloud.com>
2019-09-14 19:26:08 +0200

    improved TeX mode
    use PDF Tools to display the .pdf
    setup auto pdf refresh on compile
    
    add pdf-tex sync. This allows to jump between pdf and source file

--- tex.el
+++ tex.el
@@ -1,1 +1,17 @@
-(setq TeX-view-evince-keep-focus t)
+;; Use pdf-tools to open PDF files
+(setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+      TeX-source-correlate-start-server t)
+
+;; Update PDF buffers after successful LaTeX runs
+(add-hook 'TeX-after-compilation-finished-functions
+          #'TeX-revert-document-buffer)
+
+(setq TeX-source-correlate-method 'synctex)
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (TeX-source-correlate-mode)
+            (define-key LaTeX-mode-map (kbd "C-c C-g") 'pdf-sync-forward-search)))
+(setq TeX-source-correlate-start-server t)
+
+;; initialize pdf-tools
+(pdf-loader-install)
