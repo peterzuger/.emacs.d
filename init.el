9b89e0c4d296d0cd584750f95d9d0c313c4c77d5
Peter Züger <zueger.peter@icloud.com>
2019-10-04 15:16:55 +0000

    configure ibuffer
    ibuffer group buffers belonging to the same git repo
        ibuffer-vc does not work for me
        it does not preserve my other groups
    ibuffer dont ask if an unmodified buffer is killed
    ibuffer dont show empty groups

--- init.el
+++ init.el
@@ -86,6 +86,34 @@
 
 ;; ibuffer configuration
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+(setq ibuffer-expert t)                    ;; delete unmodified buffers without asking
+(setq ibuffer-show-empty-filter-groups nil);; dont show empty groups
+
+(defun git-root-dir(buf)
+  "Return the git root directory of BUF, or nil."
+  (when (buffer-file-name buf)
+    (locate-dominating-file (buffer-file-name buf) ".git")))
+
+(defun group-buffer-list()
+  "Return an ibuffer group for all current buffers."
+  (remove 'nil
+          (mapcar
+           (lambda(buf)
+             (when (git-root-dir buf)
+               (let ((name (git-root-dir buf)))
+                 `(,name (name . ,(buffer-name buf))))))
+           (buffer-list))))
+
+(add-hook 'ibuffer-hook
+          (lambda()
+            (ibuffer-switch-to-saved-filter-groups "default")
+            (setq ibuffer-filter-groups (append ibuffer-filter-groups (group-buffer-list)))
+            (let ((ibuf (get-buffer "*Ibuffer*")))
+              (when ibuf
+                (with-current-buffer ibuf
+                  (pop-to-buffer ibuf)
+                  (ibuffer-update nil t))))))
+
 (setq ibuffer-saved-filter-groups
       (quote (("default"
                ("dired" (mode . dired-mode))
@@ -95,10 +123,6 @@
                ("mu4e" (or
                         (mode . message-mode)
                         (mode . mail-mode)))))))
-
-(add-hook 'ibuffer-mode-hook
-          (lambda ()
-            (ibuffer-switch-to-saved-filter-groups "default")))
 
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"))
