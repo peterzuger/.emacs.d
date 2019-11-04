3b70ef003bb31c8ca161cd9896699368bc88c355
Peter Züger <zueger.peter@icloud.com>
2019-11-04 17:57:19 +0100

    fixed javascript setup
    js2-mode is not a hook list

--- javascript.el
+++ javascript.el
@@ -10,20 +10,17 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-(add-hook 'js2-mode
-          (lambda()
-            (js2-imenu-extras-mode)
-            (js2-refactor-mode)))
-
-(js2r-add-keybindings-with-prefix "C-c C-r")
-(define-key js2-mode-map (kbd "C-k") 'js2r-kill)
-
 ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
 ;; unbind it.
 (define-key js-mode-map (kbd "M-.") nil)
 
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") 'js2r-kill)
+
 (add-hook 'js2-mode-hook
-          (lambda ()
-            (add-hook 'xref-backend-functions 'xref-js2-xref-backend nil t)))
+          (lambda()
+            (add-hook 'xref-backend-functions 'xref-js2-xref-backend nil t)
+            (js2-imenu-extras-mode)
+            (js2-refactor-mode)))
 
 ;;; javascript.el ends here
