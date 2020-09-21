3c0152af6598f8f479501831ef50eabe605c989b
Peter Züger <zueger.peter@icloud.com>
2020-09-21 20:40:05 +0200

    automatically update org TODO tree's

--- org.el
+++ org.el
@@ -35,6 +35,13 @@
 (setq org-catch-invisible-edits 'smart)
 (setq org-image-actual-width nil)
 
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
 (global-set-key (kbd "C-c c") 'org-capture)
 
 (setq org-contacts-files "~/Notes/contacts.org")
