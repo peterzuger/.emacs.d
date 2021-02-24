baf906e758f9e4c8a644c3a175daf5618e5824e6
Peter Züger <zueger.peter@icloud.com>
2021-02-24 11:46:57 +0100

    org babel fix tab handling and native indentation
    prevents weird indentation and allows easy copy/paste of code snippets

--- org.el
+++ org.el
@@ -36,6 +36,9 @@
 (setq org-image-actual-width nil)
 (setq org-return-follows-link t)
 
+(setq org-src-tab-acts-natively t)
+(setq org-src-preserve-indentation t)
+
 (defun org-summary-todo (n-done n-not-done)
   "Switch entry to DONE when all subentries are done, to TODO otherwise."
   (let (org-log-done org-log-states)   ; turn off logging
