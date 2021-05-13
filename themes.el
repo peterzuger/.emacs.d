fc8f4b84d08ff74f7e17485b7d4a8aab91af14ff
Peter Züger <zueger.peter@icloud.com>
2021-05-13 12:57:52 +0200

    add all themes folders programmatically instead of individually

--- themes.el
+++ themes.el
@@ -27,39 +27,14 @@
 ;;; Commentary:
 ;;; Code:
 
-;; https://github.com/bbatsov/zenburn-emacs
-;; https://github.com/purcell/color-theme-sanityinc-tomorrow
-;; https://github.com/kuanyui/moe-theme.el
-;; https://github.com/cpaulik/emacs-material-theme
-;; https://github.com/n3mo/cyberpunk-theme.el
-;; https://github.com/m00natic/anti-zenburn-theme
-;; https://github.com/jordonbiondo/ample-theme
-;; https://github.com/alezost/alect-themes
-;; https://github.com/wasamasa/gotham-theme
-;; https://github.com/juba/color-theme-tangotango
-;; https://github.com/rexim/gruber-darker-theme
-;; https://github.com/mjwall/ample-zen
-;; https://github.com/sjrmanning/noctilux-theme
-;; https://github.com/osener/emacs-afternoon-theme
-;; https://github.com/steckerhalter/grandshell-theme
-;; https://github.com/cryon/subatomic
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/moe/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/material/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/cyberpunk/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/ample/")
-(add-to-list 'load-path              "~/.emacs.d/themes/alect/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/alect/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/gotham/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/tangotango/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/gruber/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/ample-zen/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/noctilux/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/afternoon/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/grandshell/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/blackboard/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/tron-legacy-emacs-theme/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/wilmersdorf-emacs-theme/")
+;; alect must be added to load-path too
+(add-to-list 'load-path "~/.emacs.d/themes/alect/")
+
+(let ((basedir "~/.emacs.d/themes/"))
+  (dolist (f (directory-files basedir))
+    (if (and (not (or (equal f ".") (equal f "..")))
+             (file-directory-p (concat basedir f)))
+        (add-to-list 'custom-theme-load-path (concat basedir f)))))
 
 (defvar after-load-theme-hook nil
   "Hook run after a color theme is loaded using `load-theme'.")
