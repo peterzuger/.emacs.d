3a6447f59276ed8e3921266c2ec0045dfdae4e29
Peter Züger <zueger.peter@icloud.com>
2021-05-21 21:13:53 +0200

    change mu4e setup to use use-package

--- mail.el
+++ mail.el
@@ -27,59 +27,48 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'mu4e)
+(require 'use-package)
 
-;; use mu4e for e-mail in emacs
-(setq mail-user-agent 'mu4e-user-agent)
+(use-package mu4e                       ;; an emacs-based e-mail client based on mu
+  :ensure nil
+  :bind ("C-c m" . mu4e)
+  :hook (mu4e-compose-mode-hook . flyspell-mode)
+  :config
+  (setq mail-user-agent 'mu4e-user-agent)
 
-;; set the location of my Maildir
-(setq mu4e-maildir       "~/Mail")            ;; top-level Maildir
-(setq mu4e-sent-folder   "/Sent Messages")    ;; folder for sent messages
-(setq mu4e-drafts-folder "/Drafts")           ;; unfinished messages
-(setq mu4e-trash-folder  "/Deleted Messages") ;; trashed messages
-(setq mu4e-refile-folder "/Archive")          ;; saved messages
-(setq mu4e-attachment-dir "~/Downloads/Mail") ;; attachments
+  (setq mu4e-maildir       "~/Mail")            ;; top-level Maildir
+  (setq mu4e-sent-folder   "/Sent Messages")    ;; folder for sent messages
+  (setq mu4e-drafts-folder "/Drafts")           ;; unfinished messages
+  (setq mu4e-trash-folder  "/Deleted Messages") ;; trashed messages
+  (setq mu4e-refile-folder "/Archive")          ;; saved messages
+  (setq mu4e-attachment-dir "~/Downloads/Mail") ;; attachments
 
-;; use 'fancy' non-ascii characters in various places in mu4e
-(setq mu4e-use-fancy-chars t)
+  (setq mu4e-maildir-shortcuts
+        '( ("/INBOX"            . ?i)
+           ("/Sent Messages"    . ?s)
+           ("/Deleted Messages" . ?t)
+           ("/Archive"          . ?a)
+           ("/Drafts"           . ?d)))
 
-;; attempt to show images when viewing messages
-(setq mu4e-view-show-images t)
+  (setq mu4e-use-fancy-chars t)
+  (setq mu4e-view-prefer-html t)
+  (setq mu4e-view-show-images t)
+  (setq mu4e-decryption-policy 'ask)
+  (setq mu4e-sent-messages-behavior 'sent)
+  (setq mu4e-get-mail-command "offlineimap")
+  (setq mu4e-update-interval  300)
+  (setq mu4e-compose-signature (concat user-full-name "\n"))
 
-;; ask if messages should be decrypted
-(setq mu4e-decryption-policy 'ask)
-
-;; don't save message to Sent Messages, IMAP takes care of this
-(setq mu4e-sent-messages-behavior 'sent)
-
-;; allow for updating mail using 'U' in the main view:
-(setq mu4e-get-mail-command "offlineimap")
-(setq mu4e-update-interval  300)          ;; update every 5 minutes
-
-;; setup shortcuts
-(setq mu4e-maildir-shortcuts
-      '( ("/INBOX"            . ?i)
-         ("/Sent Messages"    . ?s)
-         ("/Deleted Messages" . ?t)
-         ("/Archive"          . ?a)
-         ("/Drafts"           . ?d)))
-
-;; setup the default signature
-(setq mu4e-compose-signature (concat user-full-name "\n"))
-
-;; setup compose mode
-(add-hook 'mu4e-compose-mode-hook 'flyspell-mode)
-
-
-;; setup outgoing mail
-(require 'smtpmail)
-(setq message-send-mail-function 'smtpmail-send-it)
-(setq message-kill-buffer-on-exit t)    ;; don't keep message buffers around
-(setq starttls-use-gnutls t)
-(setq smtpmail-starttls-credentials '(("smtp.mail.me.com" 587 nil nil)))
-(setq smtpmail-auth-credentials     '(("smtp.mail.me.com" 587 user-mail-address nil)))
-(setq smtpmail-default-smtp-server     "smtp.mail.me.com")
-(setq smtpmail-smtp-server             "smtp.mail.me.com")
-(setq smtpmail-smtp-service             587)
+  (use-package smtpmail
+    :ensure nil
+    :config
+    (setq message-send-mail-function 'smtpmail-send-it)
+    (setq message-kill-buffer-on-exit t)    ;; don't keep message buffers around
+    (setq starttls-use-gnutls t)
+    (setq smtpmail-starttls-credentials '(("smtp.mail.me.com" 587 nil nil)))
+    (setq smtpmail-auth-credentials     '(("smtp.mail.me.com" 587 user-mail-address nil)))
+    (setq smtpmail-default-smtp-server     "smtp.mail.me.com")
+    (setq smtpmail-smtp-server             "smtp.mail.me.com")
+    (setq smtpmail-smtp-service             587)))
 
 ;;; mail.el ends here
